8970f2449641d9a306c8ada1468c38a483802923
Moss Prescott <moss@theprescotts.com>
2026-03-18 12:37:00 -0400

    Test macro with a second gate

--- simulator/src/tests/test_declare.rs
+++ simulator/src/tests/test_declare.rs
@@ -41,7 +41,7 @@ impl Component for TestNot {
 }
 
 #[test]
-fn test_expand_macro() {
+fn test_expand_not() {
     let chip = TestNot::chip();
     let ic = chip.expand().unwrap();
 
@@ -64,3 +64,79 @@ fn test_expand_macro() {
     assert_eq!(nand_b.id,   a.id);  // b is tied to a (it's a NOT: a NAND a)
     assert_eq!(nand_out.id, out.id);
 }
+
+/// Almost as trivial, but uses a second Nand.
+pub struct TestAnd {
+    pub a: Input,
+    pub b: Input,
+    pub out: Output,
+}
+
+impl Reflect for TestAnd {
+    fn reflect(&self) -> Interface {
+        Interface {
+            inputs:  [("a".to_string(), BusRef::from_input(self.a)),
+                      ("b".to_string(), BusRef::from_input(self.b))].into(),
+            outputs: [("out".to_string(), BusRef::from_output(self.out))].into(),
+        }
+    }
+    fn name(&self) -> String { "TestAnd".to_string() }
+}
+
+impl Chip for TestAnd {
+    fn chip() -> Self {
+        TestAnd { a: Input::new(), b: Input::new(), out: Output::new() }
+    }
+}
+
+impl Component for TestAnd {
+    type Target = Combinational<N1>;
+
+    expand! { |this| {
+        nand: Nand {
+            a: this.a,
+            b: this.b,
+            out: Output::new(),
+        },
+        not: Nand {
+            a: nand.out.into(),
+            b: nand.out.into(),
+            out: this.out,
+        }
+    }}
+}
+
+#[test]
+fn test_expand_and() {
+    let chip = TestAnd::chip();
+    let ic = chip.expand().unwrap();
+
+    assert_eq!(ic.name(), "TestAnd");
+
+    assert_eq!(ic.intf.inputs.len(), 2);
+    let a = ic.intf.inputs["a"];
+    let b = ic.intf.inputs["b"];
+
+    assert_eq!(ic.intf.outputs.len(), 1);
+    let out = ic.intf.outputs["out"];
+
+    assert_eq!(ic.components.len(), 2);
+    let Combinational::Nand(ref nand) = ic.components[0] else { panic!("expected Nand") };
+    let nand_a   = BusRef::from_input(nand.a);
+    let nand_b   = BusRef::from_input(nand.b);
+    let nand_out = BusRef::from_output(nand.out);
+
+    let Combinational::Nand(ref not) = ic.components[1] else { panic!("expected Nand") };
+    let not_a   = BusRef::from_input(not.a);
+    let not_b   = BusRef::from_input(not.b);
+    let not_out = BusRef::from_output(not.out);
+
+    assert_eq!(nand_a.id,   a.id);
+    assert_eq!(nand_b.id,   b.id);
+
+    assert_eq!(not_a.id, nand_out.id);
+    assert_eq!(not_b.id, nand_out.id);
+
+    assert_eq!(not_out.id, out.id);
+}
+
